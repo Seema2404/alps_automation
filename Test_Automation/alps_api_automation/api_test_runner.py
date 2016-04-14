eafc555240ef75eccf874150dccf3b36184f03f5
parikshitmukherjee-iquanti <parikshitmukhcyb@gmail.com>
2016-04-14 19:37:50 +0530

    Added files via upload
    Pushed Test_Automation folder structure

---
+++ Test_Automation/alps_api_automation/api_test_runner.py
@@ -0,0 +1,29 @@
+import HTMLTestRunner
+import api_projectkeywordestimated
+import unittest
+
+
+class TestRunner:
+
+    def __init__(self):
+        print "TestRunner.__INIT__"
+    #This is to launch tests and run tests for the desired plan
+    def test(args=None):
+
+        fp = file('/home/10613/Downloads/unittest-xml-reporting-2.1.0/test_result.html', 'wb')
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=fp,
+            title='API boundary tests',
+            description='This test result gives the details of the test runs of all the boundary value assertions.'
+        )
+
+
+        suite = unittest.TestSuite()
+
+        #suite.addTests(unittest.makeSuite(api_projectkeywordestimated.TestBoundaryValues))
+        suite.addTest(unittest.makeSuite(api_projectkeywordestimated.TestProjectKeywordEstimated))
+
+        runner.run(suite)
+
+    if __name__ == '__main__':
+        test();
---
+++ Test_Automation/alps_api_automation/data_collection_projectkeywordestimated.py
@@ -0,0 +1,216 @@
+
+
+
+
+
+#Test Data
+
+keyword_metrics= [
+                                 {
+                                    'keyword':'allstate life insurance',
+                                    'rank':4,
+                                    'rank_mom':-2,
+                                    'rank_prev':2,
+                                    'search_volume':567643213,
+                                    'search_volume_prev':234524.767,
+                                    'search_volume_mom_percentage':241939.772711937,
+                                    'search_volume_mom':567408688.233,
+                                    'ctr_percentage':280.05,
+                                    'estimated_traffic_mom':741535076.514021,
+                                    'estimated_traffic_mom_percentage':233305.577081022,
+                                    'estimated_traffic_prev':317838.55567949
+
+                            },
+                                {
+                                    'keyword':'all state car insurance',
+                                    'rank':2,
+                                    'rank_mom':4,
+                                    'rank_prev':6,
+                                    'search_volume':50000000,
+                                    'search_volume_prev':25988761,
+                                    'search_volume_mom_percentage':92.3908469554,
+                                    'search_volume_mom':24011239,
+                                    'ctr_percentage':300.09,
+                                    'estimated_traffic_mom':17203957.1849871,
+                                    'estimated_traffic_mom_percentage':32.5727383967,
+                                    'estimated_traffic_prev':52817042.8150129
+
+                            },
+                                {
+                                    'keyword':'homeowners insurance',
+                                    'rank':2,
+                                    'rank_mom':4,
+                                    'rank_prev':6,
+                                    'search_volume':1000001,
+                                    'search_volume_prev':6534,
+                                    'search_volume_mom_percentage':15204.576063667,
+                                    'search_volume_mom':993467,
+                                    'ctr_percentage':300.09,
+                                    'estimated_traffic_mom':1387142.33114258,
+                                    'estimated_traffic_mom_percentage':10446.0809877798,
+                                    'estimated_traffic_prev':13279.069277419
+
+                            },
+                                {
+                                    'keyword':'allstate home insurance',
+                                    'rank':4,
+                                    'rank_mom':-2,
+                                    'rank_prev':2,
+                                    'search_volume':432354,
+                                    'search_volume_prev':"NA",
+                                    'search_volume_mom_percentage':"NA",
+                                    'search_volume_mom':"NA",
+                                    'ctr_percentage':280.05,
+                                    'estimated_traffic_mom':565043.4426,
+                                    'estimated_traffic_mom_percentage':0,
+                                    'estimated_traffic_prev':0
+
+                            },
+                                {
+                                    'keyword':'allstate insurance',
+                                    'rank':1,
+                                    'rank_mom':3,
+                                    'rank_prev':4,
+                                    'search_volume':9999,
+                                    'search_volume_prev':4545,
+                                    'search_volume_mom_percentage':120,
+                                    'search_volume_mom':5454,
+                                    'ctr_percentage':450.01,
+                                    'estimated_traffic_mom':15003.599587742,
+                                    'estimated_traffic_mom_percentage':250.2782761533,
+                                    'estimated_traffic_prev':5994.767032258
+
+                            },
+                                {
+                                    'keyword':'allstate life insurance claims',
+                                    'rank':2,
+                                    'rank_mom':4,
+                                    'rank_prev':6,
+                                    'search_volume':5446.3434,
+                                    'search_volume_prev':12.868,
+                                    'search_volume_mom_percentage':42224.7078023003,
+                                    'search_volume_mom':5433.4754,
+                                    'ctr_percentage':300.09,
+                                    'estimated_traffic_mom':7601.016546319,
+                                    'estimated_traffic_mom_percentage':29065.1199237321,
+                                    'estimated_traffic_prev':26.151677909
+
+                            },
+                                {
+                                    'keyword':'allstate homeowners insurance',
+                                    'rank':2,
+                                    'rank_mom':4,
+                                    'rank_prev':6,
+                                    'search_volume':999,
+                                    'search_volume_prev':555,
+                                    'search_volume_mom_percentage':80,
+                                    'search_volume_mom':444,
+                                    'ctr_percentage':300.09,
+                                    'estimated_traffic_mom':271.091289678,
+                                    'estimated_traffic_mom_percentage':24.0344436791,
+                                    'estimated_traffic_prev':1127.92890322
+
+                            },
+                                {
+                                    'keyword':'all state auto',
+                                    'rank':4,
+                                    'rank_mom':-2,
+                                    'rank_prev':2,
+                                    'search_volume':100,
+                                    'search_volume_prev':0,
+                                    'search_volume_mom_percentage':0,
+                                    'search_volume_mom':100,
+                                    'ctr_percentage':280.05,
+                                    'estimated_traffic_mom':130.69,
+                                    'estimated_traffic_mom_percentage':0,
+                                    'estimated_traffic_prev':0
+
+                            },
+                                 {
+                                    'keyword':'all state',
+                                    'rank':1,
+                                    'rank_mom':3,
+                                    'rank_prev':4,
+                                    'search_volume':0,
+                                    'search_volume_prev':50,
+                                    'search_volume_mom_percentage':-100,
+                                    'search_volume_mom':-50,
+                                    'ctr_percentage':450.01,
+                                    'estimated_traffic_mom':-65.949032258,
+                                    'estimated_traffic_mom_percentage':-100,
+                                    'estimated_traffic_prev':65.949032258
+
+
+
+
+                            },
+                                   {
+                                    'keyword':'auto insurance',
+                                    'rank':1,
+                                    'rank_mom':3,
+                                    'rank_prev':4,
+                                    'search_volume':25.53,
+                                    'search_volume_prev':100000,
+                                    'search_volume_mom_percentage':-99.97447,
+                                    'search_volume_mom':-99974.47,
+                                    'ctr_percentage':450.01,
+                                    'estimated_traffic_mom':-131844.450324729,
+                                    'estimated_traffic_mom_percentage':-99.9593517982,
+                                    'estimated_traffic_prev':131898.064516129
+
+                            },
+                                    {
+                                    'keyword':'car insurance',
+                                    'rank':4,
+                                    'rank_mom':-2,
+                                    'rank_prev':2,
+                                    'search_volume':99999,
+                                    'search_volume_prev':2566664,
+                                    'search_volume_mom_percentage':-96.103910171,
+                                    'search_volume_mom':-2466665,
+                                    'ctr_percentage':280.05,
+                                    'estimated_traffic_mom':-3347770.27355806,
+                                    'estimated_traffic_mom_percentage':-96.2429140504,
+                                    'estimated_traffic_prev':3478458.96665806
+
+                            },
+
+
+                                 {
+                                    'keyword':'allstate car insurance quote',
+                                    'rank':1,
+                                    'rank_mom':3,
+                                    'rank_prev':4,
+                                    'search_volume':2500003400000,
+                                    'search_volume_prev':978767656565322,
+                                    'search_volume_mom_percentage':-99.7445764188,
+                                    'search_volume_mom':-976267653165322,
+                                    'ctr_percentage':450.01,
+                                    'estimated_traffic_mom':-1285725471312710,
+                                    'estimated_traffic_mom_percentage':-99.5933212195,
+                                    'estimated_traffic_prev':1290975595119530
+                            },
+
+
+
+
+
+
+
+                                 {
+                                    'keyword':'homeowners insurance florida',
+                                    'rank':1,
+                                    'rank_mom':"NA",
+                                    'rank_prev':"NA",
+                                    'search_volume':"NA",
+                                    'search_volume_prev':"NA",
+                                    'search_volume_mom_percentage':"NA",
+                                    'search_volume_mom':"NA",
+                                    'ctr_percentage':450.01,
+                                    'estimated_traffic_mom':"NA",
+                                    'estimated_traffic_mom_percentage':"NA",
+                                    'estimated_traffic_prev':"NA"
+
+                            }
+
+        ]
