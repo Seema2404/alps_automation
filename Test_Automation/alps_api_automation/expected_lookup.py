958a06f6f9c0eb376ba2712db884c6bdc6c74caa
shanusachdeva-iquanti <shanu.sachdeva@iquanti.com>
2016-04-20 13:49:20 +0530

    automation

---
+++ Test_Automation/alps_api_automation/expected_lookup.py
@@ -0,0 +1,35 @@
+#Test Data
+keyword_lookup = {
+    'allstate life insurance': {
+        'rank':1,
+        'rank_prev':2,
+        'search_volume':567643213,
+        'search_volume_prev':234524.767,
+        'estimated_traffic':741535076.514021,
+        'estimated_traffic_prev':317838.55567949
+    },
+    'all state car insurance': {
+        'rank':1,
+        'rank_prev':6,
+        'search_volume':50000000,
+        'search_volume_prev':25988761,
+        'estimated_traffic':17203957.1849871,
+        'estimated_traffic_prev':52817042.8150129
+    },
+    'homeowners insurance': {
+        'rank':2,
+        'rank_prev':6,
+        'search_volume':1000001,
+        'search_volume_prev':6534,
+        'estimated_traffic':1387142.33114258,
+        'estimated_traffic_prev':13279.069277419
+    },
+    'allstate home insurance': {
+        'rank':4,
+        'rank_prev':2,
+        'search_volume':432354,
+        'search_volume_prev':"NA",
+        'estimated_traffic':565043.4426,
+        'estimated_traffic_prev':0
+    }
+}
