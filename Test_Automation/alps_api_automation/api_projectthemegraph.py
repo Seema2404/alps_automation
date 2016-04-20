2c38564d19db38b4ced9ec2903ae95fcab30cb12
shanusachdeva-iquanti <shanu.sachdeva@iquanti.com>
2016-04-20 16:29:48 +0530

    api automation rollup, on page content theme graph

--- Test_Automation/alps_api_automation/api_projectthemegraph.py
+++ Test_Automation/alps_api_automation/api_projectthemegraph.py
@@ -1,43 +1,27 @@
-import csv
-import requests
+#http://qa5.smallbizvoices.com/alps/dashboard/iquanti/projects/85/projectthemesgraph?themes=200&month_range=13
+import utils
 import unittest
-import expected_lookup
-
-class ProjectThemeGraphAPI(unittest.TestCase):
-
- @classmethod
- def setUpClass(cls):
-    try:
-            cls.csv_file = open('projectthemegraph.csv', 'wb')
-            cls.csv_writer = csv.writer(cls.csv_file, delimiter=',')
-            cls.csv_writer.writerow(['Metric', 'Theme', 'Failure', 'Error'])
-
-            api=requests.get('http://qa5.smallbizvoices.com/alps/dashboard/iquanti/projects/224/projectthemesgraph?themes=674&month_range=13&session_token=alps_st_018mh5zlp4srdum5zuzmu3onyrm9008c')
-            cls.actual_response=api.json()['aggregated_data']
-
-    except Exception as e:
-            cls.csv_writer.writerow(['API Failure'])
-            print "API did not give any response: %s" % cls.API_URL
-
- def comparator(self,metric):
-     try:
-        if(expected_lookup.theme_rollup_lookup['april'][metric]!=self.actual_response[0][metric]):
-            self.csv_writer.writerow([metric,'Auto',str(expected_lookup.theme_rollup_lookup['april'][metric])+'!='+str(self.actual_response[0][metric])])
-        if(expected_lookup.theme_rollup_lookup['march'][metric]!=self.actual_response[1][metric]):
-            self.csv_writer.writerow([metric,'Auto',str(expected_lookup.theme_rollup_lookup['march'][metric])+'!='+str(self.actual_response[1][metric])])
-     except Exception as e:
-        self.csv_writer.writerow(['weighted_rank','Auto','','Code failure due to exception'])
+from base_api_automation import BaseALPSAPIAutomation
+from expected_lookup import keyword_lookup
 
 
+class TestProjectKeywordEstimated(BaseALPSAPIAutomation):
 
- def test_weighted_rank(self):
-     self.comparator(metric='weighted_rank')
+    API_URL = 'http://qa5.smallbizvoices.com/alps/dashboard/iquanti/projects/85/projectthemesgraph?themes=200&month_range=13&session_token=alps_st_018mh5zlp4srdum5zuzmu3onyrm9008c'
+    REPORT_FILE_NAME = 'project_keyword_estimated_api.csv'
 
- def test_total_monthly_search_volume(self):
-    self.comparator(metric='total_monthly_search_volume')
 
- def test_total_traffic(self):
-     self.comparator(metric='total_traffic')
+    def test_total_monthly_search_volume(self):
+        field = 'total_monthly_search_volume'
+        for keyword, values in keyword_lookup.iteritems():
+            actual = self.actual_lookup[keyword][field]
+            print actual
+            total_search_volume = 0
+            total_search_volume = values['search_volume']+ total_search_volume
+            print total_search_volume
+            expected = values[field] * utils.mtd_factor_calculator()
+            self.assert_actual_expected(keyword, field, actual, expected)
+
 
 
 if __name__ == '__main__':
