7829412c1377959eea61c767d6ac3315e368927d
shanusachdeva-iquanti <shanu.sachdeva@iquanti.com>
2016-04-25 18:33:10 +0530

    updated to show html report

--- Test_Automation/alps_api_automation/expected_rollup_lookup.py
+++ Test_Automation/alps_api_automation/expected_rollup_lookup.py
@@ -3,46 +3,91 @@ keyword_rollup_lookup = {
         'search_volume' : 1260,
         'share_of_search' : 9.920634920634921,
         'share_of_search_mom_percentage' : 9.920634920634921,
+        'rank_distribution' : {
+            '1' : 12,
+            '2' : 0,
+            '3' : 0,
+            '4' : 2
+        }
     },
     'car insurance' : {
         'search_volume' : 1260,
         'share_of_search' : 0.15873015873015872,
         'share_of_search_mom_percentage' : 0.0024801587301587213,
+        'rank_distribution' : {
+            '1' : 3,
+            '2' : 0,
+            '3' : 0,
+            '4' : 11
+        }
 
     },
     'chase' : {
         'search_volume' : 1260,
         'share_of_search' : 0.15873015873015872,
         'share_of_search_mom_percentage' : 0.0024801587301587213,
-
+        'rank_distribution' : {
+            '1' : 1,
+            '2' : 1,
+            '3' : 1,
+            '4' : 10
+        }
     },
     'creditcards.com' : {
         'search_volume' : 1260,
         'share_of_search' : 2.5396825396825395,
         'share_of_search_mom_percentage' : 0.03968253968253954,
-
+        'rank_distribution' : {
+            '1' : 8,
+            '2' : 0,
+            '3' : 0,
+            '4' : 6
+        }
     },
     'geico' : {
         'search_volume' : 1260,
         'share_of_search' : 6.031746031746032,
         'share_of_search_mom_percentage' : 1.8129960317460316,
+        'rank_distribution' : {
+            '1' : 2,
+            '2' : 0,
+            '3' : 0,
+            '4' : 12
+        }
     },
     'nerdwallet' : {
         'search_volume' : 1260,
         'share_of_search' : 3.968253968253968,
         'share_of_search_mom_percentage' : 0.21825396825396792,
+        'rank_distribution' : {
+            '1' : 8,
+            '2' : 4,
+            '3' : 0,
+            '4' : 2
+        }
 
     },
     'statefarm' : {
         'search_volume' : 1260,
         'share_of_search' : 0.6349206349206349,
         'share_of_search_mom_percentage' : -1.9432043650793651,
-
+        'rank_distribution' : {
+            '1' : 3,
+            '2' : 1,
+            '3' : 1,
+            '4' : 9
+        }
     },
     'value penguin' : {
         'search_volume' : 1260,
         'share_of_search' : 0.9523809523809524,
         'share_of_search_mom_percentage' : -0.7663690476190478,
+        'rank_distribution' : {
+            '1' : 6,
+            '2' : 0,
+            '3' : 0,
+            '4' : 8
+        }
 
     },
 }
