f00dbe713b20338b7074f68563a114236e6187d1
shanusachdeva-iquanti <shanu.sachdeva@iquanti.com>
2016-04-25 18:48:29 +0530

    updated to show html report

--- Test_Automation/alps_api_automation/api_projectkeywordsrollup.py
+++ Test_Automation/alps_api_automation/api_projectkeywordsrollup.py
@@ -7,7 +7,7 @@ class TestProjectKeywordRollup(BaseALPSROLLUPAPIAutomation):
     API_URL = 'http://qa5.smallbizvoices.com/alps/dashboard/iquanti/projects/85/projectkeywordsrollup?alias=0&month_range=2&keyword_types=non_branded&sort_by=alias_name&&&limit=10&offset=0&session_token=alps_st_018mh5zlp4srdum5zuzmu3onyrm9008c'
     REPORT_FILE_NAME = 'project_keyword_rollup_api.csv'
 
-    def test_rank(self):
+    def test_search_volume(self):
         field = 'search_volume'
         for alias_name, values in keyword_rollup_lookup.iteritems():
             actual = self.actual_rollup_lookup[alias_name][field]
@@ -17,9 +17,30 @@ class TestProjectKeywordRollup(BaseALPSROLLUPAPIAutomation):
                 failed_msg = 'actual: %s, expected: %s' % (actual, expected)
                 print failed_msg
                 self.write_to_csv([response['metric'], response['alias_name'], failed_msg, ''])
+                self.fail()
             elif response['is_error']:
                 print response['metric'] + response ['alias_name'] + response['error_msg']
                 self.write_to_csv([response['metric'], response['alias_name'], '', response['error_msg']])
+
+    def test_rank_distribution(self):
+        field = 'rank_distribution'
+        for alias_name, values in keyword_rollup_lookup.iteritems():
+            actual = self.actual_rollup_lookup[alias_name][field]
+            expected = values[field]
+            response = self.assert_rollup_actual_expected(alias_name,field,actual, expected)
+            if response['is_failed']:
+                failed_msg = 'actual: %s, expected: %s' % (actual, expected)
+                print failed_msg
+                self.write_to_csv([response['metric'], response['alias_name'], failed_msg, ''])
+                self.fail()
+            elif response['is_error']:
+                print response['metric'] + response ['alias_name'] + response['error_msg']
+                self.write_to_csv([response['metric'], response['alias_name'], '', response['error_msg']])
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
