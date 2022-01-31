2ead04d66de3345b4448a4ac46b3990b4663ceba
rhimanshu703 <himanshu@iquanti.com>
2022-01-31 15:50:48 +0530

    changes in test case

--- cypress/integration/sanity/pagesimulator.spec.js
+++ cypress/integration/sanity/pagesimulator.spec.js
@@ -457,6 +457,17 @@ describe('As an ALPS user', () => {
         simulationAction.shouldNotDisphowEditorWords()
     })
 
+    it('AL-T137: all the keywords of a project,sorted in the descending order of search volumes by default when user clicks on “Select from Project"', () => {
+        loginAction.clickAlpsLogo()
+        kgaAction.enterKeyword(data.SimulationKeyword)
+        kgaAction.enterURL(data.SimulationUrl)
+        kgaAction.clickGo()
+        cy.wait(9000)
+        kgaSerpAction.clickSerpPageSimulation()
+        simulationAction.clickTabProjectKeywordInSimPage()
+        //incomplete test case because bug reorted
+    })
+
     it('AL-T1320: verify are we able to see all Related KW in that section', () => {
         loginAction.clickAlpsLogo()
         simulationAction.clickTabOptimization()
